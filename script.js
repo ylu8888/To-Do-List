2a5d8d7e4e27830faf877e898cff0af72e046934
ylu8888 <yang.lu.3@stonybrook.edu>
2023-07-09 01:59:55 -0400

    fin

--- script.js
+++ script.js
@@ -7,19 +7,16 @@ document.addEventListener('DOMContentLoaded', function (){
         let task = document.querySelector('#searchbar').value;  //get value from user's search input
         let li = document.createElement('li');  //create a list item
         li.innerHTML = task;    //set list item to search input
+        document.querySelector('#tasklist').append(li); 
 
         //removing a task
-        let remove = document.createElement('button');
-        remove.innerHTML = '  X';
-        li.appendChild(remove);
-
-        remove.addEventListener('click', () => {
-            removeTask(li);
-        })
+        let span = document.createElement('span');
+        span.innerHTML = '\u00d7';
+        li.appendChild(span);
 
        
-        document.querySelector('#tasklist').append(li); 
-        document.querySelector('#searchbar').value = '';    //reset the search bar to empty
+        document.querySelector('#searchbar').value = ''; 
+        saveData();   //reset the search bar to empty
         document.querySelector('#searchbutton').disabled = true;
 
     }
@@ -28,15 +25,35 @@ document.addEventListener('DOMContentLoaded', function (){
     document.querySelector("#searchbar").onkeyup = () => {
         if(document.querySelector('#searchbar').value.length === 0){
             document.querySelector('#searchbutton').disabled = true;
+            
         }
         else{
             document.querySelector('#searchbutton').disabled = false;
+            
         }
     }
 
-    const removeTask = (li) => {
-        li.remove();
+    document.querySelector('#tasklist').addEventListener('click', function (e){
+        if(e.target.tagName === "LI"){
+            e.target.classList.toggle("checked");
+            saveData();
+        }
+        else if(e.target.tagName === "SPAN"){
+            e.target.parentElement.remove();
+            saveData();
+        }
+    });
+
+    function saveData(){
+        localStorage.setItem("data", document.querySelector('#tasklist').innerHTML);
     }
+
+    function showList(){
+        document.querySelector('#tasklist').innerHTML = localStorage.getItem("data");
+    }
+
+    showList();
+
 
     
 });
